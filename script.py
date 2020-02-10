4f2acd247aff2e23f7605dcc010c8e9c97243520
ajz123 <ajzwirn@gmail.com>
2020-02-10 08:14:16 -0800

    hi

---
+++ script.py
@@ -0,0 +1,19 @@
+import re
+
+from robobrowser import RoboBrowser
+
+br = RoboBrowser()
+br.open("https://www.capitalone.com/")
+form = br.get_form()
+form['email'] = "FILL_USERNAME_IN"
+form['password'] = "FILL_PASSWORD_IN"
+br.submit_form(form)
+
+src = str(br.parsed())
+
+start = '<li class="header-bal">Earned: '
+end = '</li>'
+
+result = re.search('%s(.*)%s' % (start, end), src).group(1)
+
+print(result)
